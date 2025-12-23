8671a85f632f72da28e80b2f7703e46c2207ad6f
Mew-72 <mayank.thakan7@gmail.com>
2025-12-23 22:42:45 +0530

    remove time management logic and associated time file

--- app/jarvis/tools/spotify.py
+++ app/jarvis/tools/spotify.py
@@ -4,19 +4,6 @@ from mcp import StdioServerParameters
 import os
 from datetime import datetime, timedelta
 
-# Read last stored time from file
-time_file = "time.txt"
-current_time = datetime.now()
-try:
-    with open(time_file, "r") as f:
-        last_time = datetime.fromisoformat(f.read().strip())
-        if last_time - current_time > timedelta(hours=6):
-            os.system("cd D:/Programming/AI/ADK AGENTS/spotify-mcp-server && npm run auth")
-except FileNotFoundError:
-    with open(time_file, "w") as f:
-        f.write(current_time.isoformat())
-
-
 spotify_mcp = McpToolset(
     connection_params=StdioConnectionParams(
         server_params=StdioServerParameters(
